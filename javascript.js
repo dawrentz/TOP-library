612d525b4af3db7fff28bbf0faef46d2fdfa6182
dawrentz <156712569+dawrentz@users.noreply.github.com>
2024-11-19 12:20:18 -0600

    Update form valiation.
    Wrote unique error messages for required used inputs in new book form.
    Trigger on bad submission and input-event.

--- javascript.js
+++ javascript.js
@@ -7,10 +7,9 @@ const addNewBookBtn = document.querySelector(".add-new-book");
 
 const submitBookBtn = document.querySelector("#submit-book-btn");
 
-
 //-----------------------------Classes-----------------------------//
 class Book {
-  constructor (title, author, pages, haveRead, notes, bookID) {
+  constructor(title, author, pages, haveRead, notes, bookID) {
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -21,7 +20,7 @@ class Book {
   }
 
   haveReadMessage() {
-   return this.haveRead ? "read" : "not read yet";
+    return this.haveRead ? "read" : "not read yet";
   }
 }
 
@@ -48,19 +47,21 @@ function createElementWithClass(elementType, className, insertText) {
   tempElement.className = className;
   if (insertText === "") {
     //nothing
-  } else {tempElement.textContent = insertText;}
+  } else {
+    tempElement.textContent = insertText;
+  }
   return tempElement;
 }
 
 //remove book from page and array
 function delBtnFunction(btn) {
   let delBookID = +btn.parentElement.getAttribute("data-book-id");
-  for(i = 0; i < myLibrary.length; i++) {
+  for (i = 0; i < myLibrary.length; i++) {
     if (myLibrary[i].bookID === delBookID) {
       myLibrary.splice(i, 1);
       break;
     }
-  } 
+  }
   btn.parentElement.remove();
 }
 
@@ -80,36 +81,46 @@ function haveReadBtnFnc(HRBtnElement, currentBook) {
 
 //create book elements and add to page
 function addBookToPage(book) {
-  const  newBookCard = createElementWithClass("div", "book-card", "");
+  const newBookCard = createElementWithClass("div", "book-card", "");
   newBookCard.setAttribute("data-book-id", book.bookID);
-  
+
   const newBookTitle = createElementWithClass("div", "title", book.title);
   const newBookAuthor = createElementWithClass("div", "author", book.author);
   const newBookPages = createElementWithClass("div", "pages", book.pages);
 
-  const newBookHaveRead = createElementWithClass("button", "haveRead", book.haveReadMessage());
-    if (book.haveRead) {
+  const newBookHaveRead = createElementWithClass(
+    "button",
+    "haveRead",
+    book.haveReadMessage()
+  );
+  if (book.haveRead) {
     newBookHaveRead.classList.add("haveReadTrue");
-  } else {newBookHaveRead.classList.add("haveReadFalse");}
-  
-  newBookHaveRead.addEventListener("click", function() {
-    haveReadBtnFnc(newBookHaveRead, book)
+  } else {
+    newBookHaveRead.classList.add("haveReadFalse");
+  }
+
+  newBookHaveRead.addEventListener("click", function () {
+    haveReadBtnFnc(newBookHaveRead, book);
   });
-  
-  const newBookNotes = createElementWithClass("div", "notes", "Notes: " + book.notes);
+
+  const newBookNotes = createElementWithClass(
+    "div",
+    "notes",
+    "Notes: " + book.notes
+  );
   const newBookDelBtn = createElementWithClass("button", "del-button", "✖");
-  
-  newBookDelBtn.addEventListener("click", function() {
-    delBtnFunction(newBookDelBtn)
+
+  newBookDelBtn.addEventListener("click", function () {
+    delBtnFunction(newBookDelBtn);
   });
-  
+
   newBookCard.appendChild(newBookTitle);
   newBookCard.appendChild(newBookAuthor);
   newBookCard.appendChild(newBookPages);
   newBookCard.appendChild(newBookHaveRead);
   newBookCard.appendChild(newBookNotes);
-  newBookCard.appendChild(newBookDelBtn);  
-  bookCards.appendChild(newBookCard);  
+  newBookCard.appendChild(newBookDelBtn);
+  bookCards.appendChild(newBookCard);
 }
 
 //add intial books on page load
@@ -118,45 +129,112 @@ myLibrary.forEach((book) => {
 });
 
 //initilize bookID after adding books to library
-let nextBookID = myLibrary.length === 0 ? 0 : myLibrary[myLibrary.length - 1].bookID + 1;
+let nextBookID =
+  myLibrary.length === 0 ? 0 : myLibrary[myLibrary.length - 1].bookID + 1;
 
 //-----------------------------Event Listeners-----------------------------//
 
 //hides/unhides form and form button
-addNewBookBtn.addEventListener("click", function() {
+addNewBookBtn.addEventListener("click", function () {
   newBookForm.classList.remove("hidden");
   addNewBookBtn.classList.add("hidden");
 });
 
 //Submit button for adding a book. Only works if form is valid
-submitBookBtn.addEventListener("click", function(event) {
-  event.preventDefault();
-  
+submitBookBtn.addEventListener("click", function (event) {
+  // event.preventDefault();
+
   if (newBookForm.checkValidity()) {
-    
     const newTitle = document.querySelector("#title").value;
     const newAuthor = document.querySelector("#author").value;
     const newPages = document.querySelector("#pages").value;
     const newHaveRead = document.querySelector("#haveRead").checked;
     const newNotes = document.querySelector("#notes").value;
     const bookID = nextBookID;
-    
+
     //problem with using new book everytime?
-    const newBook = new Book(newTitle, newAuthor, newPages, newHaveRead, newNotes, bookID);
+    const newBook = new Book(
+      newTitle,
+      newAuthor,
+      newPages,
+      newHaveRead,
+      newNotes,
+      bookID
+    );
     addBookToPage(newBook);
-    
+
     newBookForm.classList.add("hidden");
-    addNewBookBtn.classList.remove("hidden"); 
+    addNewBookBtn.classList.remove("hidden");
     newBookForm.reset();
 
     nextBookID++;
+    //no submit form (no refresh page)
+    event.preventDefault();
+  } else {
+    showError();
   }
 });
 
-//debugger
-document.querySelector("body").addEventListener("click", function() {
-  console.log("myLibrary: vvvv");
-  console.table(myLibrary);
-  console.log("nextBookID: " + nextBookID);
-  console.log("======================================================");
+//-------------Form Validation-------------//
+
+//declarations
+const newBookTitleInput = document.getElementById("title");
+const newBookAuthorInput = document.getElementById("author");
+const newBookPagesInput = document.getElementById("pages");
+
+//add error ELs
+newBookTitleInput.addEventListener("input", (event) => {
+  showError();
 });
+newBookAuthorInput.addEventListener("input", (event) => {
+  showError();
+});
+newBookPagesInput.addEventListener("input", (event) => {
+  showError();
+});
+
+//handles all error cases
+function showError() {
+  //title
+  if (newBookTitleInput.validity.tooLong) {
+    newBookTitleInput.setCustomValidity("too long!");
+  } else if (newBookTitleInput.validity.tooShort) {
+    newBookTitleInput.setCustomValidity(
+      `too short! needs ${
+        newBookTitleInput.minLength - newBookTitleInput.value.length
+      } more character(s)`
+    );
+  } else {
+    newBookTitleInput.setCustomValidity("");
+  }
+  //author
+  if (newBookAuthorInput.validity.tooLong) {
+    newBookAuthorInput.setCustomValidity("too long!");
+  } else if (newBookAuthorInput.validity.tooShort) {
+    newBookAuthorInput.setCustomValidity(
+      `too short! needs ${
+        newBookAuthorInput.minLength - newBookAuthorInput.value.length
+      } more character(s)`
+    );
+  } else {
+    newBookAuthorInput.setCustomValidity("");
+  }
+  //pages
+  if (newBookPagesInput.validity.typeMismatch) {
+    newBookPagesInput.setCustomValidity("numbers, yo...");
+  } else if (newBookPagesInput.validity.rangeOverflow) {
+    newBookPagesInput.setCustomValidity("too much! go outside!");
+  } else {
+    newBookPagesInput.setCustomValidity("");
+  }
+}
+
+//-----------------------------Testing-----------------------------//
+
+//debugger
+// document.querySelector("body").addEventListener("click", function () {
+//   console.log("myLibrary: vvvv");
+//   console.table(myLibrary);
+//   console.log("nextBookID: " + nextBookID);
+//   console.log("======================================================");
+// });
